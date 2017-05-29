e1034094c4deed20454703ba81ca96351deb9f71
chibana <chibana@camobile.com>
2017-05-29 18:36:08 +0900

    add entity validation

--- src/main/java/sample/doma/entity/Person.java
+++ src/main/java/sample/doma/entity/Person.java
@@ -6,6 +6,9 @@ import org.seasar.doma.GeneratedValue;
 import org.seasar.doma.GenerationType;
 import org.seasar.doma.Id;
 import org.seasar.doma.Table;
+import sample.validator.Fkey;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 
@@ -21,14 +24,17 @@ public class Person {
     Long id;
 
     /**  */
+    @NotNull
     @Column(name = "first_name")
     String firstName;
 
     /**  */
+    @NotNull
     @Column(name = "last_name")
     String lastName;
 
     /**  */
+    @Fkey(table = "groups")
     @Column(name = "group_id")
     Integer groupId;
 
--- src/main/java/sample/doma/entity/PersonListener.java
+++ src/main/java/sample/doma/entity/PersonListener.java
@@ -1,12 +1,6 @@
 package sample.doma.entity;
 
-import org.seasar.doma.jdbc.entity.EntityListener;
-import org.seasar.doma.jdbc.entity.PostDeleteContext;
-import org.seasar.doma.jdbc.entity.PostInsertContext;
-import org.seasar.doma.jdbc.entity.PostUpdateContext;
-import org.seasar.doma.jdbc.entity.PreDeleteContext;
-import org.seasar.doma.jdbc.entity.PreInsertContext;
-import org.seasar.doma.jdbc.entity.PreUpdateContext;
+import org.seasar.doma.jdbc.entity.*;
 
 /**
  * 
